09744d469a0dc119f93c3978322543bd15835278
Raynos <raynos2@gmail.com>
2014-03-23 23:56:15 -0700

    move create-hash to a different file

---
+++ create-hash.js
@@ -0,0 +1,22 @@
+module.exports = createHash
+
+function createHash(elem) {
+    var attributes = elem.attributes
+    var hash = {}
+
+    if (attributes === null || attributes === undefined) {
+        return hash
+    }
+
+    for (var i = 0; i < attributes.length; i++) {
+        var attr = attributes[i]
+
+        if (attr.name.substr(0,5) !== "data-") {
+            continue
+        }
+
+        hash[attr.name.substr(5)] = attr.value
+    }
+
+    return hash
+}
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var Weakmap = require("weakmap")
 var Individual = require("individual")
 
+var createHash = require("./create-hash.js")
+
 var datasetMap = Individual("__DATA_SET_WEAKMAP", Weakmap())
 
 module.exports = DataSet
@@ -11,27 +13,6 @@ function DataSet(elem) {
     if (!hash) {
         hash = createHash(elem)
         datasetMap.set(elem, hash)
-    }
-
-    return hash
-}
-
-function createHash(elem) {
-    var attributes = elem.attributes
-    var hash = {}
-
-    if (attributes === null || attributes === undefined) {
-        return hash
-    }
-
-    for (var i = 0; i < attributes.length; i++) {
-        var attr = attributes[i]
-
-        if (attr.name.substr(0,5) !== "data-") {
-            continue
-        }
-
-        hash[attr.name.substr(5)] = attr.value
     }
 
     return hash
