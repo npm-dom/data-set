e75cbf3a38e467e01228b005ef7cc5deaef5bd8e
Raynos <raynos2@gmail.com>
2014-05-03 15:00:56 -0700

    update deps

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
+var DataSet = require("./index.js")
+
 var test = require("tape")
-var h = require("hyperscript")
 var document = require("global/document")
 var element = require("element")
-
-var DataSet = require("./index.js")
 
 test("can fetch records out of DS", function (assert) {
     var elem = document.body
@@ -18,18 +17,6 @@ test("can fetch records out of DS", function (assert) {
     assert.end()
 })
 
-test("can fetch pre existing records out of other DS", function (assert) {
-    var elem = h("div", {
-        "data-foo": "bar",
-        "data-baz": { "foobaz": "oh yeah" }
-    })
-
-    var ds = DataSet(elem)
-    assert.equal(ds.foo, "bar")
-    assert.deepEqual(ds.baz, { "foobaz": "oh yeah" })
-
-    assert.end()
-})
 
 test("can fetch pre existing records out of DOM", function (assert) {
     var elem = element("<div data-foo='bar' data-baz='oh yeah'></div>")
@@ -42,9 +29,8 @@ test("can fetch pre existing records out of DOM", function (assert) {
 })
 
 test("setting dash names", function (assert) {
-    var elem = h("div", {
-        "data-foo-bar": "baz"
-    })
+    var elem = document.createElement("div")
+    DataSet(elem)["foo-bar"] = "baz"
     var elem2 = element("<div data-foo-bar='baz'></div>")
 
     var ds = DataSet(elem)
